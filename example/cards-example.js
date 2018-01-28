b20300f600924c6f66463c39178ccacee7149b27
rootslab <44gatti@gmail.com>
2018-01-28 20:33:27 +0100

    ..

--- example/cards-example.js
+++ example/cards-example.js
@@ -1,43 +1,25 @@
 /*
- * a naive implementation of the Cartesian product between combinations
+ * get all combinations of k cards chosen from n values,
+ * without replacement/repetition 
  */
 
 let log = console.log
     , Geco = require( '../' )
     , cards = 'AKQJT98765432'.split( '' )
     , concat = !!true
-    , j1 = 0
-    , n1 = 5
-    , k1 = 4
-    , iter1 = Geco.gen( n1, k1, cards, concat )
-    , j2 = 0
-    , n2 = 3
-    , k2 = 2
-    , iter2 = Geco.gen( n2, k2, cards, concat )
-    , comb = null
-    , scomb = null
-    , result = null
+    , j = 0
+    , n = 5
+    , k = 4
+    , iter = Geco.gen( n, k, cards, concat )
     ;
 
 log();
-    
 
-
-for ( comb of iter1 ) {
-    for ( scomb of iter2 ) {
-        result = comb.concat( scomb );
-        if ( concat ) result = Buffer.concat( result );
-        log( ` (${++j2}): ${comb} ${scomb} -> ${result}` );
-    }
-    iter2 = Geco.gen( n2, k2, cards, concat );
-    ++j1;
-}
+for ( const comb of iter )
+    log( ` (${++j}): ${comb}` );
 
 log();
-log( `- cards: ${ cards.slice( 0, n1 ) }` );
-log( `\n- (n1,k1): ${ n1 },${ k1 }` );
-log( `  comb: ${j1}` );
-log( `\n- (n2,k2): ${ n2 },${ k2 }` );
-log( `  comb: ${j2 / j1}` );
-log( `\n- tot comb: ${j2}` );
+log( `- cards: ${ cards.slice( 0, n ) }` );
+log( `- (n,k): ${ n },${ k }` );
+log( `- comb: ${j}` );
 log();
