11033e30086b6bb9ae1bf632aa383161bf869be8
rootslab <44gatti@gmail.com>
2018-01-28 21:02:09 +0100

    v0.1.4 added next() example..

---
+++ example/next-example.js
@@ -0,0 +1,31 @@
+/*
+ * simple next() example
+ */
+
+let log = console.log
+    , Geco = require( '../' )
+    , cards = 'AKQJT98765432'.split( '' )
+    , concat = !!true
+    , j = 0
+    , n = 5
+    , k = 4
+    , iter = Geco.gen( n, k, cards, concat )
+    , comb = null
+    ;
+
+log();
+
+while ( true ) {
+	comb = iter.next();
+	if ( comb.done ) {
+		log( ' done!');
+		break;
+	}
+	log( ` (${++j}): ${comb.value}` );
+}
+
+log();
+log( `- cards: ${ cards.slice( 0, n ) }` );
+log( `- (n,k): ${ n },${ k }` );
+log( `- comb: ${j}` );
+log();
